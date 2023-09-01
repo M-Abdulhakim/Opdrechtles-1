4900acc03c7ba587e1731dd2f60bba20ecba6e4c
M-Abdulhakim <143715405+M-Abdulhakim@users.noreply.github.com>
2023-09-01 12:23:12 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log("main.js is running");
+
+let a = 5;
+let b = 10;
+
+let Result = a + b; 
+console.log("som opdr1:", Result);
+
+a = 20;
+b = 50;
+
+Result = a + b ;
+console.log("som opdr2:", Result);
+
+let x = 25;
+let y = 45;
+
+Result = x * y ;
+console.log("som opdr2:", Result);
+
+let Salaris = 2000;
+let Raise = 0.05;
+let SalarisRaise = Salaris * Raise;
+console.log("som opdr3:", SalarisRaise);
+
+x = 15;
+y = 3;
+
+Result = x / y;
+console.log("som opdr4:", Result);
+
+
+let huisprijs = 400000;
+let precentage = 0.025;
+
+let Rente= huisprijs * precentage;
+console.log("som opdr3:", Rente);
+
+
+
+
